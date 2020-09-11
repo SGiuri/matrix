5c48ca626e63dccbba3894f1a0afbc5a53477df4
s.giuri <s.giuri@issm.it>
2020-09-11 11:04:30 +0200

    To be commented

--- matrix.py
+++ matrix.py
@@ -1,9 +1,17 @@
 class Matrix:
     def __init__(self, matrix_string):
+        # splitting each line
+        self.matrix_string = matrix_string.strip().split("\n")
         pass
 
     def row(self, index):
-        pass
+
+        # splitting
+        return [int(j) for j in self.matrix_string[index-1].split()]
+
 
     def column(self, index):
-        pass
+        my_column = []
+        for row_unsplitted in self.matrix_string:
+            my_column.append(int(row_unsplitted.split()[index-1]))
+        return my_column
