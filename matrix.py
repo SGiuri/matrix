71730ea29b1cc7c3b702871cbed1e1aee4c4aa8a
s.giuri <s.giuri@issm.it>
2020-09-11 12:56:46 +0200

    Preparing documentation

--- matrix.py
+++ matrix.py
@@ -1,17 +1,41 @@
 class Matrix:
+    """
+    A class used to represent Matrix
+
+    ...
+
+    Attributes
+    ----------
+    matrix_string : str
+        string with embedded newlines like:
+
+        "9 8 7\n5 3 2\n6 6 7"
+
+    Methods
+    -------
+    row(index)
+        returns the index row
+    """
+
     def __init__(self, matrix_string):
-        # splitting each line
-        self.matrix_string = matrix_string.strip().split("\n")
-        pass
+        """
+        Parameters
+        ----------
+        name : str
+            The name of the animal
+        sound : str
+            The sound the animal makes
+        num_legs : int, optional
+            The number of legs the animal (default is 4)
+        """
+
+        # splitting the string in double-levelled list of string
+        self.matrix_string = [row.split() for row in matrix_string.strip().split("\n")]
 
     def row(self, index):
-
-        # splitting
-        return [int(j) for j in self.matrix_string[index-1].split()]
-
+        # converting each element of "index"th row to "int" and returning it as list
+        return [int(j) for j in self.matrix_string[index - 1]]
 
     def column(self, index):
-        my_column = []
-        for row_unsplitted in self.matrix_string:
-            my_column.append(int(row_unsplitted.split()[index-1]))
-        return my_column
+        # converting each "index"th element of each row in int and returning as list
+        return [int(k[index - 1]) for k in self.matrix_string]
